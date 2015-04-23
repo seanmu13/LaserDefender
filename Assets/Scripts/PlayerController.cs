7aa3ad65ee3fc89dfeb7db27a48f21a1a6818c15
Sean Murray <seanmu13@gmail.com>
2015-04-23 19:19:14 -0400

    Lecture 102 - Restricting The Player's Position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,33 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 15.0f;
+	public float padding = 1.0f;
+
+	private float xmin;
+	private float xmax;
 
 	// Use this for initialization
 	void Start () {
-	
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if( Input.GetKey(KeyCode.LeftArrow) ) {		
-			transform.position += new Vector3(-speed * Time.deltaTime,0,0);
+		
+			transform.position += Vector3.left * speed * Time.deltaTime;
 			
-		} else if( Input.GetKey(KeyCode.RightArrow) ) {		
-			transform.position += new Vector3(speed * Time.deltaTime,0,0);
+		} else if( Input.GetKey(KeyCode.RightArrow) ) {	
 			
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}		
+		
+		// Restrict the player to the gamespace
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);		
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
 }
