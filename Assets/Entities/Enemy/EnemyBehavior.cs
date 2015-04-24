c98b40579cd9c1fb4093631ccdbf006279369da9
Sean Murray <seanmu13@gmail.com>
2015-04-23 23:11:13 -0400

    Lecture 108 - Shooting Enemies

---
+++ Assets/Entities/Enemy/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	public float health = 150f;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		
+		if(missile) {
+			health -= missile.GetDamage();
+			missile.Hit();
+			
+			if (health <= 0 ) {
+				Destroy(gameObject);
+			}
+			
+			Debug.Log("Hit by a projectile");
+		}
+	}
+}
